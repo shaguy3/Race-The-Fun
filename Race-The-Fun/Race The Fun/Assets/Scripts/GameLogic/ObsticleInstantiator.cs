14b3ce4983731913ded25c40205a5cf733f85faa
Guy Shamilyan <shaguy3@gmail.com>
2021-09-25 20:42:16 +0300

    Initial commit. Basic functionallity and UI

---
+++ Race-The-Fun/Race The Fun/Assets/Scripts/GameLogic/ObsticleInstantiator.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ObsticleInstantiator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_FirstObsticle;
+
+    [SerializeField]
+    private GameObject m_SecondObsticle;
+
+    [SerializeField]
+    private GameObject m_ThirdObsticle;
+
+    [SerializeField]
+    private GameObject m_ForthObsticle;
+
+    private List<GameObject> m_obsticles = new List<GameObject>();
+
+    private float m_obsticleSpeed;
+    private float m_obsticleTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_obsticleSpeed = 60.0f;
+        m_obsticleTimer = 3.0f;
+        
+        m_obsticles.Add(m_FirstObsticle);
+        m_obsticles.Add(m_SecondObsticle);
+        m_obsticles.Add(m_ThirdObsticle);
+        m_obsticles.Add(m_ForthObsticle);
+
+        StartCoroutine(InstantiateObsticleTimer(m_obsticleTimer));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    private IEnumerator InstantiateObsticleTimer(float i_WaitForTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(i_WaitForTime);
+            
+            var whichObsticleGenerator = new System.Random();
+            int obsticleChoice = whichObsticleGenerator.Next(m_obsticles.Count);
+            GameObject instantiatedObsticle = Instantiate(m_obsticles[obsticleChoice], new Vector3(0,0,200.0f), Quaternion.identity);
+            instantiatedObsticle.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.back * m_obsticleSpeed);
+            Destroy(instantiatedObsticle, 10.0f);
+        }
+    }
+
+    private void AdvanceLevel()
+    {
+        m_obsticleSpeed += 10.0f;
+        m_obsticleTimer -= 0.5f;
+    }
+}
